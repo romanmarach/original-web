a689a4d62815a9f7a6ee8f96545bdb13f2695c17
Roman <romanmarach@ymail.com>
2024-11-17 02:15:23 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
+import './components/NavBar.css';
+import NavBar from './components/NavBar';
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/pages/Home'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Router>
+      <NavBar />
+      <Routes>
+        <Route path='/'element = {<Home/>} />
+      </Routes>
+      </Router>
   );
 }
 
---
+++ src/components/HeroSection.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import '../App.css';
+import { Button } from './Button';
+import './HeroSection.css';
+
+
+function HeroSection() {
+  return (
+    <div className='hero-container'>
+      <video src="/waves1.mp4" autoPlay loop muted />
+      <h1>WELCOME</h1>
+      <p>.</p>
+      <div className='hero-btns'>
+        {/* <Button 
+        className='btns' 
+        buttonStyle='btn--outline'
+        buttonSize='btn--large'
+        >
+            LinkedIn
+        </Button> */}
+
+        <Button 
+        className='btns' 
+        buttonStyle='btn--primary'
+        buttonSize='btn--large'
+        >
+            Watch trailer <i className='far fa-play-circle' />
+        </Button>
+
+
+      </div>
+    </div>
+  )
+}
+
+export default HeroSection
